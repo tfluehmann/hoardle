b3ec4d9b1312b0c862a49f6ed51793787f1698fa
Tobias Fluehmann <t.fluemann94@hotmail.com>
2014-04-07 21:51:05 +0200

    + development : MVC : initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hoardle::Application.config.secret_key_base = '5d91fa4b757095f71b8909c409bd04719911b7974ecfaa2527addeec8da78a31adc6a944acf429655c8e4c9a979d6cf432b4db52f8526e2fcef37ef260230a1c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hoardle::Application.config.session_store :cookie_store, key: '_hoardle_session'
