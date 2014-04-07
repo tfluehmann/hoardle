797540330e2d3ef81a290b88833673928b70d770
Tobias Fluehmann <t.fluemann94@hotmail.com>
2014-04-07 22:59:31 +0200

    + development : mvc : conf -> added kill and restart script, migrated and added birthday column

--- db/migrate/20140116214002_devise_create_users.rb
+++ db/migrate/20140116214002_devise_create_users.rb
@@ -2,6 +2,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
       ## Database authenticatable
+      t.datetime :birthday
+      t.string :username
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
 
