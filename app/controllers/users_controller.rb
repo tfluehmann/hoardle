041c5c06581f4fe49d7c6f8c4ad87673decf63e0
Tobias Fluehmann <t.fluemann94@hotmail.com>
2014-04-15 22:07:48 +0200

    + development : mVc : added links and categories index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < Devise::RegistrationsController
   before_filter :check_permissions, :only => [:new, :create, :cancel]
   skip_before_filter :require_no_authentication
+  
+  def new
+    
+  end
+  
+  
   def check_permissions
     authorize! :create, resource
   end
