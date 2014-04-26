8f130201633fff84ba8415b7c10d1abc8ccd5479
toob1994 <t.fluehmann94@hotmail.com>
2014-04-26 14:57:34 +0200

    - : mVc : removed unused js code

--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -1,17 +1,3 @@
 $(document).ready(function(){
-	$("#test").click(function(){
-		// $.getJSON("/categories", function(data){
-			// alert(data);
-		// });
-		ajax_req();
-	});
-});
 
-function ajax_req(){
-	$.ajax("/categories", {
-		dataType: "html",
-		async: false
-	}).done(function(data){
-		alert(data);
-	});
-}
+});
