835878350c9f6c2fc5c3910e4fc6b17af0442ab0
toob1994 <t.fluehmann94@hotmail.com>
2014-07-01 00:48:08 +0200

    . development : Mvc : optimized model validation

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,5 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :categories, join_table: "categories_tags"
   has_and_belongs_to_many :links, join_table: "links_tags"
-  validates :name, presence: true
-  validates_uniqueness_of :name
+  validates :name, presence: true, uniqueness: true
 end
